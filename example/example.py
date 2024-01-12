c7a94ff381fd36858e25add24211a755026d62fb
Johnathan Georgaras <jdgeorga@login2.frontera.tacc.utexas.edu>
2024-01-11 22:39:57 -0600

    reformat

---
+++ example/example.py
@@ -0,0 +1,6 @@
+import os
+from scfcompare.file_operations import read_relax_output, get_subdirectories
+
+
+
+
--- utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Utilities Module for your_package_name.
-
-This module provides various utility functions used across the package.
-"""
-
-# Importing utility functions
-from .utilities import calculate_xy_area, extract_total_wall_time_in_seconds, add_allegro_number_array
