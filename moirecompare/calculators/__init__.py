faef71c151a5c7952fa9179eabfdb07d9abe0330
jdgeorga <jdgeorga@stanford.edu>
2024-02-22 22:19:44 -0800

    some modifications for calculators

--- moirecompare/calculators/__init__.py
+++ moirecompare/calculators/__init__.py
@@ -1,3 +1,5 @@
-from .allegro import AllegroCalculator
-from .lammps import BilayerLammpsCalculator
+#from .allegro import AllegroCalculator
+from .lammps import MonolayerLammpsCalculator, BilayerLammpsCalculator
 from .qe import QECalculator
+from .layered_lj import LayerLennardJones
+from .fingerprint import BaseFingerprintCalculator, create_moire_grid
--- moirecompare/calculators/lammps.py
+++ moirecompare/calculators/lammps.py
@@ -159,7 +159,7 @@ class BilayerLammpsCalculator(LAMMPSlib):
 
     def minimize(self, atoms, method="fire"):
         min_cmds = [f"min_style       {method}",
-                    f"minimize        0.0 1.0e-4 10000000 10000000",
+                    f"minimize        0.0 1.0e-8 10000000 10000000",
                     "write_data      lammps.dat_min"]
 
         self.set(amendments=min_cmds)
@@ -169,3 +169,90 @@ class BilayerLammpsCalculator(LAMMPSlib):
                                    'stress'],
                        system_changes=["positions"],
                        n_steps=1)
+
+
+class MonolayerLammpsCalculator(LAMMPSlib):
+    implemented_properties = ["energy", "energies", "forces", "free_energy"]
+
+    def __init__(self, atoms, chemical_symbols, system_type='TMD'):
+
+        self.system_type = system_type
+        self.original_atom_types = atoms.get_chemical_symbols()
+        self.original_masses = [atomic_masses[m] for m in [atomic_numbers[c] for c in chemical_symbols]]
+        # print(self.original_atom_types, self.original_masses)
+        print(chemical_symbols)
+
+        if self.system_type is None: 
+            print("Specify type of bilayer. Options are 'TMD' or 'graphene'")
+
+        if self.system_type == 'TMD':
+            
+            cmds = [
+                # LAMMPS commands go here.
+                "pair_style hybrid/overlay sw lj/cut 10.0",
+                f"pair_coeff * * sw tmd.sw {chemical_symbols[0]} {chemical_symbols[1]} {chemical_symbols[2]}",
+                "pair_coeff * * lj/cut 0.0 3.0",
+                "neighbor        2.0 bin",
+                "neigh_modify every 1 delay 0 check yes"]
+            # Define fixed atom types and masses for the simulation.
+
+            fixed_atom_types = {'H': 1, 'He': 2, 'Li': 3}
+            fixed_atom_type_masses = {'H': self.original_masses[0],
+                                      'He': self.original_masses[1],
+                                      'Li': self.original_masses[2]}
+            # Set up the LAMMPS calculator with the specified commands.
+
+            LAMMPSlib.__init__(self,
+                               lmpcmds=cmds,
+                               atom_types=fixed_atom_types,
+                               atom_type_masses=fixed_atom_type_masses,
+                               log_file='log.txt',
+                               keep_alive=True)
+        
+    def calculate(self, 
+                  atoms, 
+                  properties=None,
+                  system_changes=all_changes):
+
+        # atoms = rotate_to_x_axis(atoms)
+        if properties is None:
+            properties = self.implemented_properties
+
+        # self.system_type = system_type
+        
+        if self.system_type is None: 
+            print("Specify type of bilayer. Options are 'TMD' or 'graphene'")
+
+        if self.system_type == 'TMD':
+            atoms.numbers = atoms.arrays["atom_types"] + 1
+            self.propagate(atoms, properties, system_changes, 0)
+        
+
+    def minimize(self, atoms, method="fire"):
+        min_cmds = [f"min_style       {method}",
+                    f"minimize        0.0 1.0e-4 10000000 10000000",
+                    "write_data      lammps.dat_min"]
+
+        self.set(amendments=min_cmds)
+        self.propagate(atoms,
+                       properties=["energy",
+                                   'forces',
+                                   'stress'],
+                       system_changes=["positions",'pbc'],
+                       n_steps=1)
+        
+    def clean_atoms(self, atoms):
+        atoms.set_chemical_symbols(self.original_atom_types)
+        if self.started:
+            self.lmp.close()
+            self.started = False
+            self.initialized = False
+            self.lmp = None
+
+        
+    # def reset_atoms(self):
+        
+        
+    #     self.atoms.numbers = atoms.arrays["atom_types"] + 1
+    #     self.atoms.set_calculator(self)
+    #     self.atoms.calc = self
